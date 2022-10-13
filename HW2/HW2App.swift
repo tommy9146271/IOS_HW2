a67fea828ed117809108c057776b77bb278a2183
tommy9146271 <tommy9146271@icloud.com>
2022-10-13 22:19:44 +0800

    Initial Commit

---
+++ HW2/HW2App.swift
@@ -0,0 +1,17 @@
+//
+//  HW2App.swift
+//  HW2
+//
+//  Created by 李子暘 on 2022/10/13.
+//
+
+import SwiftUI
+
+@main
+struct HW2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
